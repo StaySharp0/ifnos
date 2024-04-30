408af8ee3494a02c62e5cd4380098d365f60dc3e
YongJunKim <Staysharp0@gmail.com>
2024-04-30 18:55:57 +0900

    build(ui): storybook js -> ts로 코드 변경

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,15 +1,16 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
 import path, { join, dirname } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value) {
+function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
-const config = {
+const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     getAbsolutePath("@storybook/addon-webpack5-compiler-swc"),
@@ -37,4 +38,5 @@ const config = {
     return config;
   },
 };
+
 export default config;
