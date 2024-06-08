a38f53bfd7ea4ebeb9324dc9294b21e1916963c2
YongJunKim <Staysharp0@gmail.com>
2024-06-08 12:33:36 +0900

    feat(frame-app): 프레임 애플리케이션 프로토타이핑
    TODO:
    - 애플리케이션간 정적파일 라우팅 고민 필요
    - @emotion 적용 필요

---
+++ apps/frame-app/webpack.config.js
@@ -0,0 +1,157 @@
+const path = require("path");
+const {
+  container: { ModuleFederationPlugin },
+} = require("webpack");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+module.exports = (_env, argv) => {
+  let { mode } = argv;
+
+  mode ??= "development";
+  const isDev = mode !== "production";
+  const assetGenerator = {
+    publicPath: "static/", // NOTE: CDN 생길 결우 변경 필요
+    outputPath: "static/",
+  };
+
+  const publicPath = "/static";
+
+  return {
+    mode,
+    devServer: {
+      open: true,
+      hot: true,
+      static: [
+        {
+          directory: path.join(__dirname, "public"),
+          publicPath,
+        },
+      ],
+    },
+    entry: path.resolve(__dirname, "src/main.tsx"),
+    devtool: "source-map",
+    output: {
+      filename: "index.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.[jt]sx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "swc-loader",
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+          ],
+        },
+        {
+          test: /\.svg$/i,
+          oneOf: [
+            {
+              use: [
+                {
+                  loader: "@svgr/webpack",
+                  options: {
+                    exportType: "named",
+                  },
+                },
+              ],
+              issuer: /\.[jt]sx?$/,
+              resourceQuery: { not: [/url/] },
+            },
+            {
+              type: "asset/resource",
+              resourceQuery: /url/, // *.svg?url
+              generator: assetGenerator,
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          type: "asset/resource",
+          generator: assetGenerator,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        templateParameters: {
+          lang: "ko-KR",
+          title: "ifnos",
+          publicPath,
+        },
+        template: "index.html",
+        minify: !isDev,
+      }),
+      new ESLintPlugin({
+        extensions: [".ts", ".tsx"],
+      }),
+      new ModuleFederationPlugin({
+        name: "host-frame",
+        remotes: {},
+        // shared: {
+        //   react: { eager: true, singleton: true },
+        //   "react-dom": { eager: true, singleton: true },
+        // },
+      }),
+    ].concat(
+      isDev
+        ? []
+        : [
+            new CopyPlugin({
+              patterns: [{ from: "public", to: "static" }],
+            }),
+            new MiniCssExtractPlugin(),
+          ]
+    ),
+    optimization: {
+      minimizer: [
+        new ImageMinimizerPlugin({
+          loader: true,
+          minimizer: {
+            implementation: ImageMinimizerPlugin.imageminMinify,
+            options: {
+              plugins: [
+                "imagemin-gifsicle",
+                "imagemin-mozjpeg",
+                "imagemin-pngquant",
+                "imagemin-svgo",
+              ],
+            },
+          },
+          generator: [
+            {
+              type: "asset",
+              implementation: ImageMinimizerPlugin.imageminGenerate,
+              options: {
+                plugins: [
+                  "imagemin-gifsicle",
+                  "imagemin-mozjpeg",
+                  "imagemin-pngquant",
+                  "imagemin-svgo",
+                ],
+              },
+            },
+          ],
+        }),
+      ],
+    },
+  };
+};
