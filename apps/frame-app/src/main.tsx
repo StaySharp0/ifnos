bf7b07c4a5eef47299be6f23e439b5bdbb896734
YongJunKim <Staysharp0@gmail.com>
2024-06-22 20:17:14 +0900

    refactor(frame-app): react-router-dom@6 createBrowserRouter 사용

--- apps/frame-app/src/main.tsx
+++ apps/frame-app/src/main.tsx
@@ -1,22 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import { AppBar } from "./components/layouts";
-import { ThemeStyle } from "./components/theme";
 import { GlobalStateProvider } from "./components/GlobalStateProvider";
-import { FrameRoutes } from "./router";
+import { ThemeStyle } from "./components/theme";
+import { frameRouter } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalStateProvider>
-      <BrowserRouter>
-        <ThemeStyle />
-        <AppBar />
-        <FrameRoutes />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </BrowserRouter>
+      <ThemeStyle />
+      <RouterProvider router={frameRouter} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </GlobalStateProvider>
   </React.StrictMode>
 );
--- apps/frame-app/src/router.tsx
+++ apps/frame-app/src/router.tsx
@@ -1,22 +1,67 @@
-import { Route, Routes } from "react-router-dom";
-import { Main } from "./components/layouts";
+import {
+  Link,
+  Outlet,
+  Route,
+  Routes,
+  createBrowserRouter,
+} from "react-router-dom";
+
+import { AppBar, Main } from "./components/layouts";
 
 const remotePrefix = "/apps";
 export const getRemoteAppURL = (appId: string) => `${remotePrefix}/${appId}`;
 
-export const FrameRoutes = () => {
-  return (
+export const Root: React.FC = () => (
+  <>
+    <AppBar />
     <Main>
-      <Routes>
-        <Route path={`${remotePrefix}/:appId/*`} element={<DummyRemoteApp />} />
-        <Route path="/*" element={<DummyFrameApp />} />
-      </Routes>
+      <Outlet />
     </Main>
+  </>
+);
+
+const DummyFrameApp = () => <div>나다 프레임앱</div>;
+
+const DummyRemoteApp = () => {
+  // TODO: appId 추출해서 remote app component import rendering
+  return (
+    <Routes>
+      <Route
+        path="*"
+        element={
+          <>
+            <Link to="inner/page/1">page1</Link>
+            <Link to="inner/page/2">page2</Link>
+            <Link to="inner/page/3">page3</Link>
+          </>
+        }
+      />
+    </Routes>
   );
 };
 
-const DummyFrameApp = () => <div>나다 프레임앱</div>;
-const DummyRemoteApp = () => {
-  // TODO: appId 추출해서 remote app component import rendering
-  return <div>너냐 리모트앱</div>;
-};
+/* https://github.com/remix-run/react-router/issues/10787 */
+export const frameRouter: ReturnType<typeof createBrowserRouter> =
+  createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        /* Home */
+        {
+          path: "",
+          element: <DummyFrameApp />,
+        },
+        /* FrameApp */
+        {
+          path: "*",
+          element: <DummyFrameApp />,
+        },
+        /* ThirdPartyApp */
+        {
+          path: `${remotePrefix}/:appId/*`,
+          element: <DummyRemoteApp />,
+        },
+      ],
+    },
+  ]);
