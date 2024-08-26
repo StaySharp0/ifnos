c1941164d5438b288297cee04e4d95152f6d6bf0
Yong-Jun Kim <yongjun@steppay.kr>
2024-08-26 17:25:53 +0900

    feat(auth): 로그인한 클라이언트의 id를 주입시켜주는 프로토콜 매퍼 추가 구현

--- servers/frame/auth/src/main/kotlin/com/ifnos/frame/auth/domain/keycloak/KeycloakAdminService.kt
+++ servers/frame/auth/src/main/kotlin/com/ifnos/frame/auth/domain/keycloak/KeycloakAdminService.kt
@@ -3,6 +3,7 @@ package com.ifnos.frame.auth.domain.keycloak
 import org.keycloak.admin.client.Keycloak
 import org.keycloak.admin.client.resource.RealmResource
 import org.keycloak.representations.idm.ClientRepresentation
+import org.keycloak.representations.idm.ProtocolMapperRepresentation
 import org.keycloak.representations.idm.RealmRepresentation
 import org.springframework.stereotype.Service
 
@@ -25,10 +26,11 @@ class KeycloakAdminService(
             createClient(this)
             setALlOptionalClientScope(this)
             roleService.createClientRoles(this)
+            addClientIdToToken(this)
         }
     }
 
-    fun createClient(realm: RealmResource, clientId: String = "portal-cli") {
+    fun createClient(realm: RealmResource, clientId: String = PORTAL_CLI_ID) {
         realm.clients().create(ClientRepresentation().apply {
             this.clientId = clientId
             isPublicClient = true
@@ -37,13 +39,40 @@ class KeycloakAdminService(
         })
     }
 
-    private fun setALlOptionalClientScope(realm: RealmResource, clientId: String = "portal-cli") {
+    private fun setALlOptionalClientScope(realm: RealmResource, clientId: String = PORTAL_CLI_ID) {
         val client = repo.findClientResourceByClientId(realm, clientId)
 
         client.defaultClientScopes.forEach { scope ->
             client.removeDefaultClientScope(scope.id)
             client.addOptionalClientScope(scope.id)
         }
+    }
+
+    private fun addClientIdToToken(realm: RealmResource, clientId: String = PORTAL_CLI_ID) {
+        val client = repo.findClientByClientId(realm, clientId)
+        val clientResource = repo.findClientResourceByClientId(realm, clientId)
+        val clientRepresentation = clientResource.toRepresentation()
+
+        // Protocol Mapper 생성
+        val mapper = ProtocolMapperRepresentation().apply {
+            name = "client_resource_id_mapper"
+            protocol = "openid-connect"
+            protocolMapper = "oidc-hardcoded-claim-mapper"
+            config = mapOf(
+                "claim.name" to "client_id",
+                "claim.value" to client.id,
+                "jsonType.label" to "String",
+                "id.token.claim" to "true",
+                "access.token.claim" to "true",
+                "userinfo.token.claim" to "true"
+            )
+        }
+
+        val mappers = clientRepresentation.protocolMappers.orEmpty().toMutableList()
+        mappers.add(mapper)
+        clientRepresentation.protocolMappers = mappers
+
+        clientResource.update(clientRepresentation)
     }
 
     companion object {
